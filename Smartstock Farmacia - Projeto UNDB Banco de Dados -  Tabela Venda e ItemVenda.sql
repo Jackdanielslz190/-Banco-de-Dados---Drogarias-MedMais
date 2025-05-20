42758e5db26dcb2648e773ada86e68fe42ee2af2
Daniel Marinho <163856023+Jackdanielslz190@users.noreply.github.com>
2025-05-20 20:51:01 -0300

    Add files via upload
    Inserir uma venda e Itens de Venda

---
+++ Smartstock Farmacia - Projeto UNDB Banco de Dados -  Tabela Venda e ItemVenda.sql
@@ -0,0 +1,5 @@
+INSERT INTO Venda (ID_Cliente, ID_Filial, DataVenda, ValorTotal)
+VALUES (1, 1, '2025-05-20', 59.80); 
+
+INSERT INTO ItemVenda (ID_Venda, ID_Produto, Quantidade, PrecoUnitario)
+VALUES (1, 1, 2, 29.90);
