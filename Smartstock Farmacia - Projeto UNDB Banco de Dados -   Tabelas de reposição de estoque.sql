ea611080589bb82fc2158a6974dd577cbccb71f5
Daniel Marinho <163856023+Jackdanielslz190@users.noreply.github.com>
2025-05-20 20:54:28 -0300

    Add files via upload
     Inserir PedidoReposicao
    
     Inserir ItemPedido

---
+++ Smartstock Farmacia - Projeto UNDB Banco de Dados -   Tabelas de reposição de estoque.sql
@@ -0,0 +1,5 @@
+INSERT INTO PedidoReposicao (ID_Filial, ID_Fornecedor, DataPedido, StatusPedido)
+VALUES (1, 1, '2025-05-20', 'Pendente');
+
+INSERT INTO ItemPedido (ID_Pedido, ID_Produto, Quantidade)
+VALUES (1, 1, 50);
